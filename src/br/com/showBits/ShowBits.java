9f052590366b1a4ec2cdeda4bd447b4720674bb8
Livio <azevedols.livio@gmail.com>
2019-01-15 10:52:07 -0200

    Criada classe ShowBits

---
+++ src/br/com/showBits/ShowBits.java
@@ -0,0 +1,33 @@
+package br.com.showBits;
+
+public class ShowBits {
+	
+	int numbits;
+	
+	ShowBits (int n){
+		numbits = n;
+	}
+	
+	void show(long val) {
+		long mask = 1;
+
+		// desloca um 1 para esquerda para a posição apropriada
+		mask <<= numbits - 1;
+
+		int spacer = 0;
+
+		for (; mask != 0; mask >>>= 1) {
+			if ((val & mask) != 0)
+				System.out.print("1");
+			else
+				System.out.print("0");
+			spacer++;
+			if ((spacer % 8) == 0) {
+				System.out.print(" ");
+				spacer = 0;
+			}
+		}
+		System.out.println();
+	}
+
+}
